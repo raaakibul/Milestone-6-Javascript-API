9ec619f5e6f06ff8b25f31eac86c5387441ff82f
rakibul-islam <raakibul.uap@gmail.com>
2023-08-23 19:46:38 +0600

    more api

--- Module-33/api.js
+++ Module-33/api.js
@@ -14,39 +14,83 @@
 // }
 
 
-function loadData(){
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-         .then(response => response.json())
-         .then(data =>console.log(data))
-}
+// function loadData(){
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//          .then(response => response.json())
+//          .then(data =>console.log(data))
+// }
 
  
 
-function loadTitle(){
+// function loadTitle(){
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//          .then(response=>response.json())
+//          .then(title =>console.log(title.title))
+// }
+
+// function loadUser(){
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(res => res.json())
+//         .then(users => displayUser(users))
+// }
+
+// function displayUser(users){
+//     const ul = document.getElementById('userId');
+//     for(const user of users){
+//         console.log(user);
+//         const li = document.createElement('li');
+//         li.innerText = `name:${user.name}, email:${user.email}` ;
+//         ul.appendChild(li);
+
+//     }
+// }
+
+// function post(){
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//         .then(response => response.json())
+//         .then(post =>console.log(post))
+// }
+
+
+// const user = {name : 'John', email : 'raakibul@gmail.com', password :' lkasflka'}
+
+// const strigified = JSON.stringify(user);
+// console.log(user);
+// console.log(strigified);
+
+
+function loadData(){
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-         .then(response=>response.json())
-         .then(title =>console.log(title.title))
+    .then(response => response.json())
+    .then(data => console.log(data.userId));
 }
 
 function loadUser(){
     fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(users => displayUser(users))
+    .then(response => response.json())
+    .then(userData =>displayUsers(userData));
 }
 
-function displayUser(users){
+function displayUsers(userData){
     const ul = document.getElementById('userId');
-    for(const user of users){
-        console.log(user);
+    for(const user of userData){
         const li = document.createElement('li');
-        li.innerText = `name:${user.name}, email:${user.email}` ;
+        li.innerText = user.name;
         ul.appendChild(li);
-
     }
 }
 
-function post(){
+function loadPost(){
     fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(post =>console.log(post))
+    .then(response => response.json())
+    .then(postData => postedData(postData));
+}
+
+function postedData(postData) {
+    ul = document.getElementById('post-id');
+    for(const post of postData){
+        const li = document.createElement('li');
+        li.innerText = post.title;
+        ul.appendChild(li);
+    }
 }
