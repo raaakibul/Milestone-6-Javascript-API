3f8bff41aef6c6641a3ea0e9d702f3a6d268c5c7
rakibul-islam <raakibul.uap@gmail.com>
2023-08-23 17:44:34 +0600

    file changes

---
+++ Module-32/inheritecnce.js
@@ -0,0 +1,16 @@
+class studentCare{
+    name = "Adjkshkj";
+    id = "123";
+    address = "bd";
+
+    constructor(name, id, address){
+        this.name = name;
+        this.id = id;
+        this.address = address;
+    }
+    
+    buildRoutine(){
+        console.log(this.name, 'build a routine');
+    }
+}
+
