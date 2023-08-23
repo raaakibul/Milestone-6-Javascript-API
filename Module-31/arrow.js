3868bf20fc1213cdfc55f2c8f3f01aaf35acb427
rakibul-islam <raakibul.uap@gmail.com>
2023-08-23 17:20:52 +0600

    class

--- Module-31/arrow.js
+++ Module-31/arrow.js
@@ -1,11 +1,16 @@
-const add = (num1, num2) => num1 + num2;
-const sum = add(22,90);
-console.log(sum);
+// const add = (num1, num2) => num1 + num2;
+// const sum = add(22,90);
+// console.log(sum);
 
-const mul = (num1, num2,num3) => num1*num2*num3;
-const multiply = mul(4,16,4);
-console.log(multiply);
-const getName = () =>'Brandon Clark'
+// const mul = (num1, num2,num3) => num1*num2*num3;
+// const multiply = mul(4,16,4);
+// console.log(multiply);
+// const getName = () =>'Brandon Clark'
 
-const name = getName()
-console.log(name);
+// const name = getName()
+// console.log(name);
+
+
+const add = (num1,num2) => num1+ num2;
+const addition = add(10,22);
+console.log(addition);
